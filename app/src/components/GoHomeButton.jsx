0f53cac331bb5abbc8c08eab016be06f840b7919
Jordit <jordidiaz2020@gmail.com>
2025-03-12 21:23:10 -0400

    Complete Assignment

--- app/src/components/GoHomeButton.jsx
+++ app/src/components/GoHomeButton.jsx
@@ -1,13 +1,14 @@
-// TODO: 
+// TODO:
 // - Import the Link component from React Router
 // - Make the button navigate the user back to the home page
+import { Link } from "react-router-dom";
 
 const GoHomeButton = () => {
   return (
-    <button className="ui button fluid">
+    <Link to="/" className="ui button fluid">
       Go Home
-    </button>
-  )
-}
+    </Link>
+  );
+};
 
 export default GoHomeButton;
